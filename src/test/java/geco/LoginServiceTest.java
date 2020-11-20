e16593ea7da85acbd963e268f193ba420e565ee1
SandraAlfaro <sandra.zgz.311@gmail.com>
2020-11-20 09:13:32 +0100

    Creation class LoginServiceTest

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,18 @@
+package geco;
+
+import junit.framework.TestCase;
+
+public class LoginServiceTest extends TestCase {
+
+    public void testLoginExists() {
+    }
+
+    public void testAddLogin() {
+    }
+
+    public void testFindAllLoginsStartingWith() {
+    }
+
+    public void testFindAllLogins() {
+    }
+}
